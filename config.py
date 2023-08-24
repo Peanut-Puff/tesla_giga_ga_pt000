5499513ab30d7f255761d2cd4682d2f26b3201e1
nono <97236799+Peanut-Puff@users.noreply.github.com>
2023-08-24 13:10:40 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# mysql 配置
+DATABASE_CONFIG = {
+    'host': '',
+    'database': 'tesla_giga_ga_pt000',
+    'user': '',
+    'password': '',
+    'port': 1,
+    'charset':''
+}
+
+PREDICT_DIR=''
+COLLECT_DIR=''
+MODEL_DIR=''
+TRAIN_DIR=''
